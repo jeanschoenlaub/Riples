701aa9359efd90ed60d09b5915feb593bb4e203a
product-team-5b <product.data@5b.com.au>
2023-08-14 15:51:13 +1000

    added clerck basic sign-in

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
+import { ClerkProvider } from '@clerk/nextjs'
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+
 };
 
 export default api.withTRPC(MyApp);
