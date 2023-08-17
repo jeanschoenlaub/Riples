c3b167312481b783f53d0c686c69bbcde9f105b9
Jean personal <jeanschoen@hotmail.com>
2023-08-17 15:31:51 +1000

    Home page UI tweaks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
-import { api } from "~/utils/api";
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { RouterOutputs, api } from "~/utils/api";
+import { SignInButton, SignOutButton, auth, useUser } from "@clerk/nextjs";
 
 const NavBar = () => {
   const {user} = useUser()
+  console.log(user)
 
   return ( 
     <div id="global-nav" className="flex justify-center w-full">
@@ -29,8 +30,7 @@ const NavBar = () => {
         />
       </div>
 
-      <div id="global-nav-mid" className="flex w-1/2 p-4  gap-3 border border-slate-700">
-      </div>
+      <div id="global-nav-mid" className="flex w-1/2 p-4  gap-3 border border-slate-700"></div>
         
       <div id="global-nav-right" className="flex w-1/4 p-4 gap-3 border border-slate-700">
         {user?.imageUrl && 
@@ -59,14 +59,63 @@ const Feed = () => {
   return ( 
     <div>
     {/* proj get all data */}
-    {data?.map((project) => (
-      <div className="border-b border-slate-700 p-4" key={project.id}>
-        {project.title}
-      </div>
+    {data?.map((fullProject) => (
+      <ProjectView {...fullProject}></ProjectView>
     ))}
   </div>
   )
 } 
+
+type ProjectWithUser = RouterOutputs["projects"]["getAll"][number]
+const ProjectView = (props: ProjectWithUser) => {
+  const {projects, author} = props;
+  const { data, isLoading } = api.projects.getAll.useQuery();
+
+  if (isLoading ) return(<div> Loading ... </div>)
+
+  if (!data) return(<div> Something went wrong</div>)
+
+  const getImagePath = (ripleType: string) => {
+    if (ripleType === 'solo') {
+      return '/solo_riple.png';
+    } else {
+      return '/multi_riple.png';
+    }
+  };
+
+  return (
+    <div id="riple-card" className="border-b border-slate-700 p-4" key={projects.id}>
+      <div id="riple-card-metadata"  className="flex items-center border-b border-e border-t border-l border-slate-300 p-2 g-4 justify-between rounded-full  bg-white ">
+        <div id="riple-card-metadata-auth-profile-image" className="flex gap-2">
+          <img 
+            src={author?.imageUrl} 
+            alt="Profile Image" 
+            className="h-10 w-10 rounded-full"
+          />
+          <div id="riple-card-metadata-auth-name-and-created-date">
+            <div className="font-bold text-gray-800"> {author?.firstName} {author?.lastName} </div>
+            <div className="text-sm text-gray-400">{projects.createdAt.toDateString()}</div>
+          </div>
+        </div>
+
+        <div className="flex-shrink-0">
+          <div id="riple-card-riple-type">
+            <img 
+              src={getImagePath(projects.ripleType)} 
+              alt="Riple Type"
+              className="h-10 w-10 rounded-full"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div id="riple-card-body" className="p-4  bg-white border-b border-e border-t border-l border-slate-300 rounded-lg">
+        {projects.title}
+      </div>
+    </div>
+  );
+}
+
 
 export default function Home() {
   return (
@@ -85,7 +134,7 @@ export default function Home() {
         <NavBar></NavBar>
     </div>
 
-    <div className="flex justify-center w-full">
+    <div className="flex justify-center w-full bg-sky-50">
         <div id="quick-links" className="flex flex-col w-1/4 p-4 border border-slate-700">
             <h1>Quick Links</h1>
         </div>
--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -1,8 +1,38 @@
+import { clerkClient} from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+    return {
+      id: user.id,
+      username: user.username,
+      firstName:user.firstName,
+      lastName:user.lastName,
+      imageUrl: user.imageUrl
+    }
+}
+
 export const projRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.project.findMany();
+  getAll: publicProcedure.query( async ({ ctx }) => {
+    const projects = await ctx.prisma.project.findMany({
+      take:100,
+    });
+
+    const user = (await clerkClient.users.getUserList({
+      userId: projects.map((project) => project.authorID),
+      limit: 100,
+    })).map(filterUserForClient);
+
+    return projects.map((projects) => {
+      const author = user.find((user) => user.id === projects.authorID)
+
+      if(!author) throw new TRPCError ({code:"INTERNAL_SERVER_ERROR", message?:"Riple author not found"})
+      return{
+        projects,
+        author,
+      }
+    });
   }),
 });
