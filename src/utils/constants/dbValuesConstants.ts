b7528e4517e3ba0c284504decd16c5fc42c876d9
Jean personal <jeanschoen@hotmail.com>
2024-01-06 12:17:42 +0700

    made the task of latest due clickable

--- src/utils/constants/dbValuesConstants.ts
+++ src/utils/constants/dbValuesConstants.ts
@@ -7,7 +7,7 @@ export const TASK_STATUS_COLORS = ["bg-green-500", "bg-yellow-500", "bg-orange-4
 // Function to get the corresponding color for a status
 export const getTaskStatusColor = (status: string) => {
     const index = TASK_STATUS_VALUES.indexOf(status);
-    return TASK_STATUS_COLORS[index] || "bg-gray-500"; // Default color if status not found
+    return TASK_STATUS_COLORS[index] ?? "bg-gray-500"; // Default color if status not found
 };
 
 // Array for project status names
@@ -19,5 +19,5 @@ export const PROJECT_STATUS_COLORS = ["bg-green-500", "bg-yellow-500", "bg-orang
 // Function to get the corresponding color for a status
 export const getProjectStatusColor = (status: string) => {
     const index = PROJECT_STATUS_VALUES.indexOf(status);
-    return PROJECT_STATUS_COLORS[index] || "bg-gray-500"; // Default color if status not found
+    return PROJECT_STATUS_COLORS[index] ?? "bg-gray-500"; // Default color if status not found
 };
